69f795c1222c69bde4d7e5c0cb79d4435735153e
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-07-16 13:00:33 -0300

    Initial Commit

---
+++ Hercules/HerculesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerculesApp.swift
+//  Hercules
+//
+//  Created by Gabriel Ferreira de Carvalho on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HerculesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
