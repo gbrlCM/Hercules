66c890457d4b675efc9d296d88313f1729104757
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-08-02 23:29:53 -0300

    Criação de exercicios

--- Hercules/Screens/WorkoutCreation/Views/Components/ExerciseCell.swift
+++ Hercules/Screens/WorkoutCreation/Views/Components/ExerciseCell.swift
@@ -18,19 +18,41 @@ struct ExerciseCell: View {
                     .font(.title3)
                 Spacer()
             }.padding(.horizontal, 16)
-            HStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))], alignment: .leading, spacing: 4) {
                 ForEach(exercise.tags, id: \.name) { tag in
-                    VStack(alignment: .leading) {
-                        Text(LocalizedStringKey(tag.name))
-                            .font(.subheadline.bold())
-                            .foregroundColor(tag.color)
+                    VStack {
+                        VStack {
+                            Text(LocalizedStringKey(tag.name))
+                                .font(.subheadline.bold())
+                                .foregroundColor(tag.color)
+                        }
+                        .frame(minWidth: 80)
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 6)
+                        .background(Capsule().stroke(tag.color))
+                        .padding(.bottom, 2)
                     }
-                    .padding(.vertical, 6)
-                    .padding(.horizontal, 10)
-                    .background(Capsule().stroke(tag.color))
                 }
-                Spacer()
             }.padding(.horizontal, 16)
         }
     }
 }
+
+struct ExerciseCell_Preview: PreviewProvider {
+    static var previews: some View {
+        ExerciseCell(exercise: Exercise())
+    }
+}
+
+/*
+ ForEach(exercise.tags, id: \.name) { tag in
+     VStack {
+         Text(LocalizedStringKey(tag.name))
+             .font(.subheadline.bold())
+             .foregroundColor(tag.color)
+     }
+     .padding(.vertical, 6)
+     .padding(.horizontal, 10)
+     .background(Capsule().stroke(tag.color))
+ }
+ */
